039064a1395c66f7045b02e72df0ca9b0a5a1c24
박동수 <pds020130@gmail.com>
2025-12-08 03:55:46 +0900

    I worked on a userLogin

--- src/main/java/com/tradenova/security/JwtAuthenticationFilter.java
+++ src/main/java/com/tradenova/security/JwtAuthenticationFilter.java
@@ -38,12 +38,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter { //OncePerReq
 
             Optional<User> optionalUser = userRepository.findByEmail(email); // email을 findByEmail로 실제 DB에 있는지 유저 조회 / optional은 null 일 수도 있고, 아닐 수도 있는 값을 감싸놓은 박스, 일반 객체로 만들었을 때 null이라면 다른 .getName() 형식 등의 코드가 있을 때 예외 터짐
             if(optionalUser.isPresent()){ //isPresent는 Optional 클래스의 메서드, Optional 안에 값이 있으면 true, 없으면 false
-                User user = optionalUser.get();
+                User user = optionalUser.get(); //값이 있으니까 get 으로 실제 User 엔티티 꺼내오기
 
-                //간단하게 ROLE 하나만 사용
+                /*
+                 * new UsernamePasswordAuthenticationToken(principal, credentials, authorities)
+                 * principal → user → “이 요청의 주인(로그인한 사용자)은 이 User 객체다”
+                 * credentials → null → 자격 증명(비밀번호). 여기서는 더 이상 필요 없어서 null
+                 * authorities → null → 사용자의 권한 목록(ROLE_USER, ROLE_ADMIN 등). 지금은 아직 권한을 안 넣어줘서 null로 둔 상태.
+                 */
+                // “이 User를 주체(principal)로 하는 인증 객체(auth)를 하나 만들자.
                 var auth = new UsernamePasswordAuthenticationToken(
                         user, null, null
-                ); //
+                );
+                //이제부터 이 요청은 이 사용자(auth) 로 인증된 상태라고 스프링 시큐리티에게 알려줘라
                 SecurityContextHolder.getContext().setAuthentication(auth);
             }
         }
